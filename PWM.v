a2f3ef6259817669fb1a580536f958a878be8aa5
Yendri González Rodríguez <yendri.gonzalez.08@outlook.com>
2015-10-01 19:05:38 -0600

    Version3: PWM, se cambió al número de bits que van a capturarse de la salida de los filtros .

--- PWM.v
+++ PWM.v
@@ -1,34 +1,18 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    21:51:13 09/25/2015 
-// Design Name: 
-// Module Name:    PWM1 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+
 //////////////////////////////////////////////////////////////////////////////////
-module SimplePWM(
+module Modulo_PWM(
 	input clk_in,						//clock for counter
-	input [7:0] x_in, 				//control value that
+	input [11:0] Dato, 				//control value that
 										//defines pulse width
 	output reg PWM_out = 1);		//PWM signal out
 
-	reg [7:0] counter = 0;
+	reg [11:0] counter = 0;
 	
 	always@ (posedge clk_in )
 		begin
-			if ( counter < x_in )
+			if ( counter < Dato )
 				PWM_out <= 1;
 			else
 				PWM_out <= 0;
