966850e12a500d167924d9414082af5a192303c4
Yendri González Rodríguez <yendri.gonzalez.08@outlook.com>
2015-10-01 18:35:04 -0600

    Version4 : Recepcion de datos del ADC digital. Se cambió la máquina de estados que rige la recepcion de datos. Ahora se controla con el CS y el SCLK va 22 veces más rápido que el CS (en bajo).

--- ADC_Recepcion.v
+++ ADC_Recepcion.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    23:00:58 09/20/2015 
+// Create Date:    17:24:05 09/28/2015 
 // Design Name: 
-// Module Name:    ADC_4 
+// Module Name:    ADC4
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,112 +18,87 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module ADC_4(
-
-input wire CS,SCLK,reset, ///Clock de la con la 40 la frecuencia de muestreo y reset
-
-input wire ADCdata,rx_en,/// Datos que provienen del ADC, Clock del Adc y habilitador de recibimiento de datos
-
-output reg rx_done_tick, ///Bandera que indica que ya todos los datos fueren recibidos
-
-output reg [15:0] b_reg, ///Variable donde se almacenan los 16 datos provenientes del ADC
-
-output wire [11:0] data_out///Datos que realmente necesito del ADC
-
+module ADC_Recepcion(
+input wire SDATA, reset ,CS,SCLK,
+output reg rx_done_tick, 
+output reg [15:0] b_reg,
+output wire [11:0]data_Out
     );
 
-//Symbolic state declaration
-
 localparam [1:0]
-	idle = 2'b00,
-	dps= 2'b01,   ///Estado de la maquina
-	load= 2'b10;
+	DetectaCS = 2'b00,
+	Recibir = 2'b01,		//Estados de la máquina
+	Carga = 2'b10;
 
-///Declaracion de las seÃ±ales
+	//Declaración de Señales
 
-reg [1:0] state_reg,state_next; ///Variables para cambiar de  estado
+	reg[1:0] state_reg , state_next; // cambiar estado
+	reg [3:0] n_reg, n_next; /// Para llevar cuenta de datos recibidos
+	reg [15:0] b_next;
 
-
-reg [3:0] n_reg,n_next;  /// Leavan la cuenta de cuatos datos se han lamacenado, deben se nueve 12 datos,
-//por lo que en cada ciclo n se le ira disminuyendo un 1
-
-reg [15:0] b_next; ///Variable donde se almacenan los datos provenientes del ADC
-
-///MÃ¡quina de estados que se encarga de recibir los datos
-
-always @(posedge reset , posedge SCLK)    /////Aqui se inicializan los estados y se actualizan las variables
-	if (reset)
-		begin
-		state_reg <= idle;
-		n_reg <= 4'd0;
-		b_reg <= 16'd0;
-		end
-	else
-		begin
-			state_reg <= state_next;
-			n_reg <= n_next;
-			b_reg <= b_next;
-
-		end
+/////Partes secuencial
+	always @(posedge reset,  negedge SCLK)
+		if (reset)
+			begin
+				state_reg <= DetectaCS;
+				n_reg <= 4'd0;
+				b_reg <= 16'd0;
+			end
+		else
+			begin 
+				state_reg <= state_next;
+				n_reg <= n_next;
+				b_reg <= b_next;
+			end
 
 ///Parte combinacional
 
 always @*
-begin
-	state_next = state_reg; //Se actualiza la variable de estado
-	rx_done_tick=1'b0;  //Se incializa la bandera
-	n_next=n_reg; //Se actualiza la variable que lleva la cuenta de las variables que son empaquetadas
-	b_next=b_reg; /// Aqui se almacenan los datos que son enviados del ADC
+ begin
+ 	state_next =state_reg;
+ 	rx_done_tick = 1'b0;
+ 	n_next = n_reg;
+ 	b_next = b_reg;
+
 
 	case(state_reg)
 
-		idle:
-			if(rx_en & ~CS)
-
+		DetectaCS :
+			if(~CS)
 				begin
-
-				//Shift in star bit
-
-				b_next = {b_reg[14:0],ADCdata};//Se mete el primer cero en el paquete de datos
-
-				n_next=4'd14; //Se pone el valor 13 en la variable para luego ser decrementado, de forma que se tenga un control de los datos que son
-				//empaquetados
-
-				state_next =dps;  //Se pasa al estado dps
-			
-
+				state_next = Recibir;
+					n_next = 4'd0;
 				end
 
-		dps: //3 ceros + 12 data 
+			else state_next = DetectaCS;
 
-				begin
-					b_next= {b_reg[14:0],ADCdata}; //Se empaquetan los 3 ceros y los 12 datos que se desean toamr del ADC
 
-					if(n_reg==4'd0)
-
-						state_next =load;
-
-					else
-						n_next = n_reg-4'd1;
-
-				end
-
-		load: // extra clock to complete the last shift
-
+		Recibir :
 			begin
-				state_next =idle;
-				rx_done_tick='b1;
-
+				b_next = {b_reg [14:0], SDATA}; /// se llena registro desplazamiento
+				if(n_reg == 4'd15)
+					state_next = Carga;
+				else 
+					n_next = n_reg + 4'd1;
 			end
+
+		Carga : 
+			 	if(CS)
+					begin
+						state_next= DetectaCS;
+						rx_done_tick = 1'b1;
+					end
+			 	else 
+			 		state_next = Carga;
+					
+
+		default  			 		
+				state_next = DetectaCS;
+	endcase
 	
-		default 
-		
-			state_next =idle;
-			
-		endcase
+end
+	
 
-	end
-
-	assign data_out = b_reg[11:0]; //Aqui se asigan los 12 datos que se necesitan del ADC
+assign data_Out = b_reg [11:0];
 
 endmodule
