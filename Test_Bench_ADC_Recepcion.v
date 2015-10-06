47bc0627419ec38a52c6a1afc1f1b9a64cbb6e38
Yendri González Rodríguez <yendri.gonzalez.08@outlook.com>
2015-10-05 20:08:19 -0600

    Test Bench para la version 4 de la recepción de datos ADC.

--- Test_Bench_ADC_Recepcion.v
+++ Test_Bench_ADC_Recepcion.v
@@ -5,66 +5,73 @@
 module Test_ADC_Recepcion;
 
 	// Inputs
-	reg SCLK;
+	reg SDATA;
 	reg reset;
-	reg ADCdata;
-	reg rx_en;
+	reg CS;
+	reg SCLK;
 
 	// Outputs
 	wire rx_done_tick;
 	wire [15:0] b_reg;
-	wire [11:0] data_out;
+	wire [11:0] data_Out;
 
 	// Instantiate the Unit Under Test (UUT)
 	ADC_Recepcion uut (
-		.SCLK(SCLK), 
+		.SDATA(SDATA), 
 		.reset(reset), 
-		.ADCdata(ADCdata), 
-		.rx_en(rx_en), 
+		.CS(CS), 
+		.SCLK(SCLK), 
 		.rx_done_tick(rx_done_tick), 
 		.b_reg(b_reg), 
-		.data_out(data_out)
+		.data_Out(data_Out)
 	);
-	integer i,j;
-	reg[15:0] datos_txt;
-	reg [15:0] Memoria [0:15]; ///Datos que se encuentran en el txt
+
+integer i,j;
+	reg [15:0] data_txt;
+	reg [15:0] Memoria [0:15];
+	reg enable_CS;
 
 	initial begin
 		// Initialize Inputs
-		SCLK = 0;
 		reset = 1;
-		ADCdata = 0;
-		rx_en = 1;
-		$readmemb("Datos_MarkV.txt",Memoria);
-	repeat(5) @( negedge SCLK)
-
+		SDATA = 0;
+	
+		SCLK = 0;
+		CS = 1;
+		enable_CS = 0;
+		$readmemb("Datos.txt",Memoria);
+	repeat(5) @(negedge SCLK)
 		reset=0;
-	
-	end
-
-	initial begin
-
-		@(negedge reset)
-			for(j=0;j<16;j=j+1)
-				begin
-				datos_txt=Memoria[j];
-				for(i=0;i==15;i=i+1)
-					begin
-
-						@(negedge SCLK)
-						ADCdata=datos_txt[i];
-
-					end
-
-			ADCdata=1;
-				end
-	end
-	
-
-initial forever begin
-	#5 SCLK=~SCLK;
-
 	end
       
-endmodule
+     initial begin
+     	@(negedge reset, negedge CS)
+     		for(j=0;j<16;j=j+1)
+     			begin
+     			data_txt=Memoria[j];
+			repeat(2)@(negedge SCLK)
+				enable_CS = 1;
+			
+     		for (i=0;i==15;i=i+1)
+     			begin
 
+     			@(negedge SCLK)
+     			SDATA=data_txt[i];
+     			end
+     		SDATA=1;
+			enable_CS = 0;
+
+     			end
+     	end
+
+ initial begin
+ @(negedge enable_CS)
+	while (enable_CS)
+		#1000 CS = ~ CS;
+ end
+
+ initial forever begin
+  #25 SCLK = ~ SCLK;
+ end
+
+endmodule
