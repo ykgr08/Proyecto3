14e1beef7371d33fec4db743ad58966fbde156b8
Yendri González Rodríguez <yendri.gonzalez.08@outlook.com>
2015-10-05 20:05:38 -0600

    Test Bench para la version 3 de la recepción de datos ADC.

--- Test_Bench_ADC_Recepcion.v
+++ Test_Bench_ADC_Recepcion.v
@@ -2,7 +2,6 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-
 module Test_ADC_Recepcion;
 
 	// Inputs
@@ -10,15 +9,11 @@ module Test_ADC_Recepcion;
 	reg reset;
 	reg ADCdata;
 	reg rx_en;
-	reg temp_finish;
-
 
 	// Outputs
-	wire En_temp;
 	wire rx_done_tick;
 	wire [15:0] b_reg;
 	wire [11:0] data_out;
-	wire CS;
 
 	// Instantiate the Unit Under Test (UUT)
 	ADC_Recepcion uut (
@@ -26,65 +21,50 @@ module Test_ADC_Recepcion;
 		.reset(reset), 
 		.ADCdata(ADCdata), 
 		.rx_en(rx_en), 
-		.temp_finish(temp_finish), 
-		.En_temp(En_temp), 
 		.rx_done_tick(rx_done_tick), 
 		.b_reg(b_reg), 
-		.data_out(data_out), 
-		.CS(CS)
+		.data_out(data_out)
 	);
+	integer i,j;
+	reg[15:0] datos_txt;
+	reg [15:0] Memoria [0:15]; ///Datos que se encuentran en el txt
 
-		integer i,j;
-	reg [15:0] datos_txt;
-	reg [15:0] Memoria [0:15];
-	reg enable_timer;
 	initial begin
 		// Initialize Inputs
-		SCLK= 0;
+		SCLK = 0;
 		reset = 1;
-		temp_finish=0;
 		ADCdata = 0;
 		rx_en = 1;
-		enable_timer=0;
 		$readmemb("Datos_MarkV.txt",Memoria);
-	repeat(5) @(posedge SCLK)
+	repeat(5) @( negedge SCLK)
+
 		reset=0;
+	
 	end
 
 	initial begin
-		@(negedge reset, posedge SCLK)
+
+		@(negedge reset)
 			for(j=0;j<16;j=j+1)
 				begin
 				datos_txt=Memoria[j];
+				for(i=0;i==15;i=i+1)
+					begin
 
-				repeat(32)@(posedge SCLK)
-				enable_timer=1;
-			for (i=0;i==15;i=i+1)
-				begin
+						@(negedge SCLK)
+						ADCdata=datos_txt[i];
 
-				@(posedge SCLK)
-				ADCdata=datos_txt[i];
-				end
+					end
+
 			ADCdata=1;
-			enable_timer=0;
-
 				end
-
-		end
-
-initial begin
-	@(posedge enable_timer)
-	while(enable_timer)
-	#22700 temp_finish=~temp_finish;
 	end
+	
 
 initial forever begin
+	#5 SCLK=~SCLK;
 
-#709 SCLK =~SCLK;
-
-end
+	end
       
 endmodule
-      
-
 
