e75a43bc59fe04d18ff6b83077941aa44c4a32df
Yendri González Rodríguez <yendri.gonzalez.08@outlook.com>
2015-10-13 01:16:03 -0600

    Módulo pre-PWM (Versión2) : A la salida del filtro se le suma 0,5 y se verifica los casos en donde puede haber underflow u overflow. Además que se trunca a 11 bits , los más significativos de la parte fraccionaria.

--- Truncador_Suma.v
+++ Truncador_Suma.v
@@ -1,12 +1,11 @@
 `timescale 1ns / 1ps
-
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
 // 
-// Create Date:    22:17:26 10/05/2015 
+// Create Date:    00:11:26 10/13/2015 
 // Design Name: 
-// Module Name:    Truncador_Suma
+// Module Name:    Truncador_Suma 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -17,32 +16,17 @@
 // Revision: 
 // Revision 0.01 - File Created
 // Additional Comments: 
-// Se utiiza 1 bit de signo , 10 parte entera y 18 fracción
+//// 1 signo, 10 entera,18 fraccion
 //////////////////////////////////////////////////////////////////////////////////
-// RESOLUCION 18  //// Ancho de palabra 29 /// 10 entera
 module Truncador_Suma(
-    input [28:0] Dato_Filtro,
-	 input listo,
-	 output [11:0] Dato_Truncado,
-	 output [28:0] QuitarW);
-	
-reg [17:0] dato_temp ;	
-	 
-always @(posedge listo)
+	input [28:0] Dato_Filtro,
+	output [10:0] resultado);
 
-begin
- if ( Dato_Filtro [17:0] == 18'b100000000000000000)   
-														 
-			 dato_temp = 18'b111111111111111111;  
-  
-  else
-		
-			 dato_temp = {~{Dato_Filtro[17]},Dato_Filtro[16:0]}; 
-		
-end
-	
-assign Dato_Truncado = dato_temp [17:6]; 
-assign QuitarW = Dato_Filtro;
+wire signo = {12{Dato_Filtro[28]}} ;
+
+assign resultado = ( Dato_Filtro[28] == 1 && signo != Dato_Filtro [28:17])? {11'b00000000000}:
+						 ( Dato_Filtro[28] == 0 && signo != Dato_Filtro [28:17])? {11'b11111111111}:
+						  {~{Dato_Filtro[17]},Dato_Filtro[16:7]};
 
 
-endmodule 
+endmodule
