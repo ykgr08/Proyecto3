c9fb955c14d1b7e05ce33c07573087567d382f7d
Yendri González Rodríguez <yendri.gonzalez.08@outlook.com>
2015-10-13 00:55:52 -0600

    Version4 : PWM. Se cambió a 11 bits el dato de entrada, porque a 100MHz es lo máximo que se puede muestrear para obtener la salida con la modulación del ancho de pulsos . El clk_in va a trabajar a 100MHz.

--- PWM.v
+++ PWM.v
@@ -1,21 +1,37 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:03:06 10/04/2015 
+// Design Name: 
+// Module Name:    PWM 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
 //////////////////////////////////////////////////////////////////////////////////
 module Modulo_PWM(
-	input clk_in,						//clock for counter
-	input [11:0] Dato, 				//control value that
-										//defines pulse width
-	output reg PWM_out = 1);		//PWM signal out
+	input clk_in,							//clock for counter
+	input [10:0] Dato, 					//control value that
+												//defines pulse width
+	output reg PWM_out = 1'b1);		//PWM signal out
 
-	reg [11:0] counter = 0;
+	reg [10:0] counter = 11'd0;
 	
 	always@ (posedge clk_in )
 		begin
 			if ( counter < Dato )
-				PWM_out <= 1;
+				PWM_out <= 1'b1;
 			else
-				PWM_out <= 0;
-				counter <= counter+1;
+				PWM_out <= 1'b0;
+				counter <= counter + 11'd1;
 		end
 endmodule
